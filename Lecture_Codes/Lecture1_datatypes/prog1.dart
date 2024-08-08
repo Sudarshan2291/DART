3c7404b2424c99c6dcd4180e9f7f968d13d321cc
Sudarshan Hanumant Khopade <73271648+Sudarshan2291@users.noreply.github.com>
2024-08-08 13:38:06 +0530

    Add files via upload

---
+++ Lecture_Codes/Lecture1_datatypes/prog1.dart
@@ -0,0 +1,13 @@
+// Program Code 1
+
+void main(){
+  int x = 10;
+  double y = 40.5;
+  print(x);
+  print(y);
+
+  x = 50.5;           // Error: A value of type 'double' can't be assigned to a variable of type 'int'.
+  y = 30;
+  print(x);
+  print(y);
+} 
